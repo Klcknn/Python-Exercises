99ff79f6cfc87926e8b6a1751e578277135f8186
Kenan KILIC <96704212+Klcknn@users.noreply.github.com>
2023-07-15 02:16:32 +0300

    usage of self for class

--- Day_1.py
+++ Day_1.py
@@ -1,1 +1,34 @@
 # Day 1 Python Exercises 
+
+# Define a class 
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+
+person_1 = Person("John",25)
+name_of_person_1 = person_1.name
+print(name_of_person_1)
+
+
+         
+
+
+class Person2:
+    def __init__(self,n,a):
+        self.name = n
+        self.age = a
+
+
+person_2 = Person2("Tomm",30)
+name_of_person_2 = person_2.name
+age_of_person_2 = person_2.age
+print(name_of_person_2,age_of_person_2)
+
+
+
+
+         
+
+
