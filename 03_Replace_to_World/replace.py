9aea654b5d71746685a62968b208ea740b17c2ff
Kenan KILIC <96704212+Klcknn@users.noreply.github.com>
2023-07-29 14:15:13 +0300

    commit

--- 03_Replace_to_World/replace.py
+++ 03_Replace_to_World/replace.py
@@ -15,7 +15,7 @@ def replace_world():
 replace_world()
  
 """
-
+""" 
 # 2. Method
 
 def replace_world(main_word):
@@ -29,4 +29,20 @@ def replace_world(main_word):
         print("Error..")
 
 entered_word = "Feel The Heat With This Quiz On Worldwide Words For Summer And Winter"
+replace_world(entered_word)
+
+ """
+# 3. Method
+
+def replace_world(main_word):
+    word_to_replace = input("Please enter a word to replace: ")
+    word_to_replacement = input("Please enter a replacement: ")
+
+    if word_to_replace.isalpha() == True and word_to_replace.isalpha()== True:
+        result_word= main_word.replace(word_to_replace, word_to_replacement)
+        print(result_word)
+    else:
+        print("Error..")
+
+entered_word = input("Enter any of the following words: ")
 replace_world(entered_word)
