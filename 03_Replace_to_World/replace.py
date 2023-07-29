254a1ac929d02a433e3b8153b998497653b4875c
Kenan KILIC <96704212+Klcknn@users.noreply.github.com>
2023-07-29 10:23:51 +0300

    update

--- 03_Replace_to_World/replace.py
+++ 03_Replace_to_World/replace.py
@@ -1,3 +1,6 @@
+# 1. Method
+""" 
+
 def replace_world():
     main_word = "The world you are trying to connect to is already in use by another user."
     word_to_replace = input("Please enter a word to replace: ")
@@ -10,3 +13,20 @@ def replace_world():
         print("Error..")
 
 replace_world()
+ 
+"""
+
+# 2. Method
+
+def replace_world(main_word):
+    word_to_replace = input("Please enter a word to replace: ")
+    word_to_replacement = input("Please enter a replacement: ")
+
+    if word_to_replace.isalpha() == True and word_to_replace.isalpha()== True:
+        result_word= main_word.replace(word_to_replace, word_to_replacement)
+        print(result_word)
+    else:
+        print("Error..")
+
+entered_word = "Feel The Heat With This Quiz On Worldwide Words For Summer And Winter"
+replace_world(entered_word)
