6d4d82d5e52d73dd54b71b5769fbe31fd4cf9e81
Kenan KILIC <96704212+Klcknn@users.noreply.github.com>
2023-07-30 15:10:38 +0300

    update

--- 04_Basic_Calculator/calculator.py
+++ 04_Basic_Calculator/calculator.py
@@ -31,38 +31,50 @@ class Calculator:
     def print_states(self, x):
         self.x = x
         print(f"Yapılan işlemin sonucu {x}'tir.")
-            
-girilen_islem = input("Yapmak istediğiniz işlemi belirtilen örnek karakterlerden birini seçiniz ( +, -, *, / ) :  ")
-if girilen_islem == "+":
-    print("Toplama işlemi yapmak istediğiniz iki değeri giriniz:  ")
-    value_1 = int(input("Birinci değeri giriniz:"))
-    value_2 = int(input("İkinci değeri giriniz:"))
-    calculator_obje = Calculator(value_1, value_2)
-    toplama_degeri=calculator_obje.addition()
-    calculator_obje.print_states(toplama_degeri)
-elif girilen_islem == "-":
-    print("Çıkartma işlemi yapmak istediğiniz iki değeri giriniz:  ")
-    value_1 = int(input("Birinci değeri giriniz:"))
-    value_2 = int(input("İkinci değeri giriniz:"))
-    calculator_obje = Calculator(value_1, value_2)
-    cikarma_degeri=calculator_obje.subraction()
-    calculator_obje.print_states(cikarma_degeri)
-elif girilen_islem == "*":
-    print("Çarpma işlemi yapmak istediğiniz iki değeri giriniz:  ")
-    value_1 = int(input("Birinci değeri giriniz:"))
-    value_2 = int(input("İkinci değeri giriniz:"))
-    calculator_obje = Calculator(value_1, value_2)
-    carpma_degeri=calculator_obje.multiply()
-    calculator_obje.print_states(carpma_degeri)
-elif girilen_islem == "/":
-    print("Bölme işlemi yapmak istediğiniz iki değeri giriniz:  ")
-    value_1 = int(input("Birinci değeri giriniz:"))
-    value_2 = int(input("İkinci değeri giriniz:"))
-    calculator_obje = Calculator(value_1, value_2)
-    bölme_degeri=calculator_obje.divide()
-    calculator_obje.print_states(bölme_degeri)
-else:
-    print("Hataa!! Farklı bir işlemde bulundunuz yapmış olduğunuz işlemi kontrol edip tekrardan giriniz...")
+
+while True:
+    print("**********************BİLGİ****************************")
+    print("Toplama işlemini yapmak için ( + ) karakterini yazınız.")                
+    print("Çıkarma işlemini yapmak için ( - ) karakterini yazınız.")                
+    print("Çarpma  işlemini yapmak için ( * ) karakterini yazınız.")                
+    print("Bölme   işlemini yapmak için ( / ) karakterini yazınız.")
+    print("Çıkış   işlemini yapmak için (E-e) karakterini yazınız.")
+
+    girilen_islem = input("Yapmak istediğiniz işlemi yukarıda belirtilen örnek karakterlerden birini yazarak devam ediniz :  ")
+    if girilen_islem == "+":
+        print("Toplama işlemi yapmak istediğiniz iki değeri giriniz:  ")
+        value_1 = int(input("Birinci değeri giriniz:"))
+        value_2 = int(input("İkinci değeri giriniz:"))
+        calculator_obje = Calculator(value_1, value_2)
+        toplama_degeri=calculator_obje.addition()
+        calculator_obje.print_states(toplama_degeri)
+    elif girilen_islem == "-":
+        print("Çıkartma işlemi yapmak istediğiniz iki değeri giriniz:  ")
+        value_1 = int(input("Birinci değeri giriniz:"))
+        value_2 = int(input("İkinci değeri giriniz:"))
+        calculator_obje = Calculator(value_1, value_2)
+        cikarma_degeri=calculator_obje.subraction()
+        calculator_obje.print_states(cikarma_degeri)
+    elif girilen_islem == "*":
+        print("Çarpma işlemi yapmak istediğiniz iki değeri giriniz:  ")
+        value_1 = int(input("Birinci değeri giriniz:"))
+        value_2 = int(input("İkinci değeri giriniz:"))
+        calculator_obje = Calculator(value_1, value_2)
+        carpma_degeri=calculator_obje.multiply()
+        calculator_obje.print_states(carpma_degeri)
+    elif girilen_islem == "/":
+        print("Bölme işlemi yapmak istediğiniz iki değeri giriniz:  ")
+        value_1 = int(input("Birinci değeri giriniz:"))
+        value_2 = int(input("İkinci değeri giriniz:"))
+        calculator_obje = Calculator(value_1, value_2)
+        bölme_degeri=calculator_obje.divide()
+        calculator_obje.print_states(bölme_degeri)
+    elif girilen_islem == "e" or girilen_islem == "E":
+        print("İşlem tamamen sonlandırıldı..Exiting...")
+        quit()
+    else:
+        print("Hataa!! Farklı bir işlemde bulundunuz yapmış olduğunuz işlemi kontrol edip tekrardan giriniz...")
+
 
 
 
